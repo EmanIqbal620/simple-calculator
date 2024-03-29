d590bc5dccafdc1b273cda63dd1588dd61f7ea6a
Eman__Iqbal <160255293+programmerEI@users.noreply.github.com>
2024-03-29 21:55:32 +0500

    Add files via upload
    my project simple calculator

---
+++ index.ts
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+//printing a Wellcome Message
+console.log("\n\tWellcome To \ CodeWithEman\ CLI Simple Calculator\n");
+
+// Asking Questions from users through Inquirer
+
+const answers = await inquirer.prompt([
+    {message: "Enter First Number", type: "number", name: "firstNumber"},
+    { message: "Enter Second Number", type: "number", name: "secondNumber"},
+    {
+        message: "Select  one Operator to perform Operations",
+        type: "list", 
+        name: "operator",
+        choices: ["Addition","subtraction","Multiplication", "Division"],
+    },
+]);
+//conditional statment If-Else
+
+if(answers.operator ==="Addition") {
+    console.log(answers.firstNumber + answers.secondNumber)
+}
+else if (answers.operator ==="subtraction"){
+    console.log(answers.firstNumber - answers.secondNumber)
+}
+else if (answers.operator === "Multiplication"){
+    console.log(answers.firstNumber * answers.secondNumber)
+}
+else if (answers.operator === "Division"){
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else{
+    console.log("invalid Input");
+};
+
+
